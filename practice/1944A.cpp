d55325c914da529c0b97cf250d44315cf7263d30
Labhesh <labheshkalbhande2406@gmail.com>
2024-04-04 20:04:09 +0530

    cp

---
+++ practice/1944A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	if (k >= (n - 1)) {
+		cout << 1 << endl;
+	}
+	else {
+		cout << n << endl;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
