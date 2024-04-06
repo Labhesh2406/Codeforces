cf1f085d0619244a40baad640fd3024ae4b27923
Labhesh <labheshkalbhande2406@gmail.com>
2024-04-06 09:09:21 +0530

    special characters

--- practice/1948A.cpp
+++ practice/1948A.cpp
@@ -1,44 +1,27 @@
 #include <bits/stdc++.h>
-#define pn cout<<"NO\n";
 using namespace std;
-
-void test() {
-	int n;
-	cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	int curr = 0;
-	for (int i = 0; i < n; i++) {
-		if (arr[i] < curr) {
-			pn return;
-		}
-		else {
-			int x = arr[i] / 10;
-			int y = arr[i] % 10;
-			if (x >= curr) {
-				if (y >= x) {
-					curr = y;
-				}
-				else {
-					curr = arr[i];
-				}
-			}
-			else {
-				curr = arr[i];
-			}
-		}
-	}
-	cout << "YES" << endl;
-}
 
 int main(int argc, char const *argv[])
 {
 	int t;
 	cin >> t;
 	while (t--) {
-		test();
+		int n ;
+		cin >> n;
+		if (n % 2) {
+			cout << "NO" << endl;
+			continue;
+		}
+		char ch = 'A';
+		string ans = "";
+		while (n) {
+			ans = ans + ch;
+			ans = ans + ch;
+			ch++;
+			n = n - 2;
+		}
+		cout << "Yes" << endl;
+		cout << ans << endl;
 	}
 	return 0;
 }
