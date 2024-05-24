adf7f425f2cca335d4bd40965da4e9ebf53f3ee9
Labhesh <labheshkalbhande2406@gmail.com>
2024-05-24 20:48:13 +0530

    two  friend

--- contest/EduRound165A.cpp
+++ contest/EduRound165A.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
+
+int solve() {
+	int ans = 3;
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 1; i <= n; i++) {
+		if (i == arr[arr[i - 1] - 1]) {
+			ans = 2;
+			break;
+		}
+	}
+	cout << ans << endl;
+}
 
 int main(int argc, char const *argv[])
 {
 	int t;
-	int a = 0;
 	cin >> t;
 	while (t--) {
-		int n;
-		cin >> n;
-		vector<int> arr(n);
-		for (int i = 0; i < n; i++) {
-			cin >> arr[i];
-			arr[i]--;
-		}
-		vector<bool> visited(n, false);
-		int min_invite = 0;
-
-		for (int i = 0; i < n; i++) {
-			if (!visited[i]) {
-				int current = i;
-				int cnt = 0;
-				while (!visited[current]) {
-					visited[current] = true;
-					current = arr[current];
-					cnt = cnt + 1;
-				}
-				min_invite += max(0, cnt - 1);
-			}
-		}
-		cout << min_invite << endl;
+		solve();
 	}
 	return 0;
 }
