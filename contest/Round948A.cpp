4be7221a4e9fab1115de661934a764a8a45fda25
Labhesh <labheshkalbhande2406@gmail.com>
2024-05-26 21:29:27 +0530

    little NIKITA

---
+++ contest/Round948A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+		if (n >= m && (n - m) % 2 == 0) {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
