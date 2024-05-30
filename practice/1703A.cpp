4af4139d69b2e9fd8078f9f464a8cac7d4535e73
Labhesh <labheshkalbhande2406@gmail.com>
2024-05-30 18:18:22 +0530

    YES or YEs ?

---
+++ practice/1703A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		if (s == "YES" || s == "yes" || s == "Yes" || s == "yEs" || s == "yeS" || s == "YEs" || s == "yES" || s == "YeS") {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
