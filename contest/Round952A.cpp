bcb74be55a67a3301be83aa33d820abfb3594e21
Labhesh <labheshkalbhande2406@gmail.com>
2024-06-12 13:56:06 +0530

    Creating Words

--- contest/Round952A.cpp
+++ contest/Round952A.cpp
@@ -4,17 +4,19 @@ using namespace std;
 void solve() {
 	string a, b;
 	cin >> a >> b;
-	if (a[0] == b[0] && a[1] == b[1] && a[2] == b[2]) {
-		cout << a << " " << b << endl;
-	}
-	else if (a[0] != b[0] && a[1] != b[1] && a[2] != b[2]) {
-		swap(a[0], b[0]);
-		cout << a << " " << b << endl;
-	}
-	else {
-		swap(a[0] , b[0]);
-		cout << a << " " << b << endl;
-	}
+
+
+	// if (a[0] == b[0] && a[1] == b[1] && a[2] == b[2]) {
+	// 	cout << a << " " << b << endl;
+	// }
+	// else if (a[0] != b[0] && a[1] != b[1] && a[2] != b[2]) {
+	// 	swap(a[0], b[0]);
+	// 	cout << a << " " << b << endl;
+	// }
+
+	swap(a[0] , b[0]);
+	cout << a << " " << b << endl;
+
 }
 
 int main(int argc, char const *argv[])
