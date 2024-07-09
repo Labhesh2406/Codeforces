52121749c3c73d5c215c823988bf9734bac92170
Labhesh <labheshkalbhande2406@gmail.com>
2024-07-09 20:05:54 +0530

    Array Divisibility

---
+++ practice/1983A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << " ";
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
