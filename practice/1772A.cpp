d17673992b6fa15ce55974a4e1c6eecabb594341
Labhesh <labheshkalbhande2406@gmail.com>
2024-07-24 22:37:06 +0530

    A+B?

---
+++ practice/1772A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b;
+		char c;
+		cin >> a >> c >> b;
+		int sum = 0;
+		cout << a + b << endl;
+	}
+	return 0;
+}
