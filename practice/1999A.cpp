570fa42ac58d3b824c4b618c0146a1a96f97a063
Labhesh <labheshkalbhande2406@gmail.com>
2024-08-07 09:44:40 +0530

    A+B Again?

---
+++ practice/1999A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int ans = n / 10 + n % 10;
+		cout << ans << endl;
+	}
+	return 0;
+}
