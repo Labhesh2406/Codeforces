3a9740e70195f1ba143fb6e89727fef817dec71b
Labhesh <labheshkalbhande2406@gmail.com>
2024-09-22 22:52:23 +0530

    Jagged Swaps

---
+++ contest/1896A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	if (v[0] == 1) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
