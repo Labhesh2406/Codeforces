0444cb6d62fe9e0afd2ad148d2981ca0662ba112
Labhesh <labheshkalbhande2406@gmail.com>
2026-01-23 18:41:27 +0530

    C - Shifted MEX  || O(nlog n) solution

--- C_Shifted_MEX.cpp
+++ C_Shifted_MEX.cpp
@@ -8,19 +8,32 @@ void solve() {
         cin >> arr[i];
     }
     sort(arr.begin(), arr.end());
-    int ans = 0;
-    for(int i = 0; i < n; ++i) {
-        int mx = 0, val = arr[i];
-        for(int j = i; j < n; ++j) {
-            if(val == arr[j] ) {
-                mx++; val++;
-            }
-        }
-        ans = max(ans, mx);
-    }
-    cout << ans << endl;
-}
+//     int ans = 0;
+//     for(int i = 0; i < n; ++i) {
+//         int mx = 0, val = arr[i];
+//         for(int j = i; j < n; ++j) {
+//             if(val == arr[j] ) {
+//                 mx++; val++;
+//             }
+//         }
+//         ans = max(ans, mx);
+//     }
+//     cout << ans << endl;
+// }
 
+    int chain = 1;
+    int ans = 1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == arr[i-1] + 1) {
+            chain++;
+        }
+        else if(arr[i] != arr[i-1]) {
+            chain = 1;
+        }
+        ans = max(ans, chain);
+    }
+    cout << ans << "\n";
+}
 
 int main() {
     ios::sync_with_stdio(false);
