7bd8af5b93d0aa3227e6f123ca3b5aeac5cac819
Labhesh <labheshkalbhande2406@gmail.com>
2026-02-10 23:42:25 +0530

    2155A - El fucho

---
+++ A_El_fucho.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void solve() {
+    ll n; cin >> n;
+    int res = 2 * n - 2;
+    cout << res << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
