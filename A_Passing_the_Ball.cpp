57faf0327cdc5e769c2497f7a9fbb41832397a59
Labhesh <labheshkalbhande2406@gmail.com>
2026-03-16 22:40:12 +0530

    2204A - Passing the Ball

---
+++ A_Passing_the_Ball.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void solve() {
+    ll n;
+    string s;
+    cin >> n >> s;
+    int first_L = s.find('L');
+    cout << (first_L + 1) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
