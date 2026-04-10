90ff088a46d89d82eef04140cf19c58e5c552566
Labhesh <labheshkalbhande2406@gmail.com>
2026-04-10 11:57:09 +0530

    1373A - Donut Shops

---
+++ A_Donut_Shops.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void solve() {
+    ll a, b, c; cin >> a >> b >> c;
+    if(a < c) {
+        cout << 1 << " ";
+    } else {
+        cout << -1 << " ";
+    }
+    if(b * a > c) {
+        cout << b <<" ";
+    } else {
+        cout << -1 << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
