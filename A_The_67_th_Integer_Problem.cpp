e3def8c6a6cc0ae00b1534283100bb031c3727e2
Labhesh <labheshkalbhande2406@gmail.com>
2026-04-11 10:28:46 +0530

    2218A - The 67th Integer Problem

---
+++ A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void solve() {
+    ll n; cin >> n;
+    cout << 67 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
