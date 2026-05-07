d1dba17aa82ad33123a3a4f7591b58cfe60d9a92
Labhesh <labheshkalbhande2406@gmail.com>
2026-05-07 19:37:05 +0530

    2226A - Disturbing Distribution

---
+++ A_Disturbing_Distribution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using ll = long long;
+const ll mod = 676767677;
+using namespace std;
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> a(n + 1); 
+    for(int i = 1; i<= n; i++) cin >> a[i];
+    ll ans = 0;
+    if(a[n] == 1) ans++;
+    for(int i = 1; i <= n ; i++) {
+        if(a[i] == 1) continue;
+        ans += a[i];
+    }
+    cout << ans % mod << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
