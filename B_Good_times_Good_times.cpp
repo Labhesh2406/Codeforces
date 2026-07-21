90cb522cd8741e9a58f2b64675440fbd01492bab
Labhesh <labheshkalbhande2406@gmail.com>
2026-07-21 11:33:46 +0530

    Alternative solutions for B_Good_times_Good_times.cpp

--- B_Good_times_Good_times.cpp
+++ B_Good_times_Good_times.cpp
@@ -3,13 +3,19 @@ using ll = long long;
 using namespace std;
 
 void solve() {
-    string x; cin >> x;
+    /* string x; cin >> x;
     cout << "1";
     x.pop_back();
     for(char c : x)
         cout << "0";
         cout << "1";
     cout << "\n";
+    */
+
+    // Alternative solution
+   ll x, y; cin  >> x;
+   for(y = 10; y <= 1e9; y *= 10) if (y > x) break;
+   cout << (y + 1) << "\n";
 }
 
 int main() {
